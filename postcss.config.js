708259a80c3316317c903870fc3d01daac488322
seducerooo <>
2023-11-18 13:05:57 +0330

    laravelMixInstalltion merged with main branch successfully

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-};
+const mix = require('laravel-mix');
+
+mix.postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+]);
